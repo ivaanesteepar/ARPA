b535d0370d4cfc266fc756b81316a3879f364921
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2023-10-16 22:10:10 +0200

    Comunicación punto a punto

--- Practica1/practica1.cpp
+++ Practica1/practica1.cpp
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+    // Inicialización de variables
     int rango, tamano, num;
+    
     MPI_Status status;
-
-
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rango);
     MPI_Comm_size(MPI_COMM_WORLD, &tamano);
