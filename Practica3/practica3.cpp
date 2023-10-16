0bf945af86fd81e326a489fe11b6afd8ba9c48aa
Iván Estépar <102294523+ivaanesteepar@users.noreply.github.com>
2023-10-16 22:11:50 +0200

    Producto escalar entre 2 vectores

--- Practica3/practica3.cpp
+++ Practica3/practica3.cpp
@@ -4,7 +4,9 @@
 #include <time.h>
 
 int main(int argc, char* argv[]) {
+    // Inicialización de variables
     int rank, size;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
